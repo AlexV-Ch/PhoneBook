f143a9d885d57fb8d246e479a845fedaa7c571d9
auto_1 <alexch@yandex.ru>
2025-02-21 11:48:34 +0300

    11.9 Практическая работа. Map - этап 01

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Создание телефонной книги
+        PhoneBook phoneBook = new PhoneBook();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("Введите номер, имя или команду:");
+            String textIn = new Scanner(System.in).nextLine();
+        //Вызов метода для textIn
+            phoneBook.setPhoneBook(textIn);
+
         }
     }
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class PhoneBook {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhoneBook {
+    HashMap<String, String> phoneBook = new HashMap<>();
+    String regexName = "[a-zA-Zа-яА-Я]+";
+    String regexPhone = "[0-9]+";
+
+    public void setPhoneBook(String textIn) {
+        if (textIn.matches(regexName)) {
+            addName(textIn);
+        } else if (textIn.matches(regexPhone)) {
+            addPhone(textIn);
+        } else {
+            System.out.println("Проверьте введенный техт");
+        }
+        //addPhone(textIn);
+        System.out.println(phoneBook.toString());
+    }
+
+    private void addPhone(String textIn) {
+        if (!phoneBook.containsKey(textIn)) {
+            System.out.println("Введите имя абонента для номера " + textIn);
+            String name = new Scanner(System.in).nextLine();
+            phoneBook.put(textIn, name);
+        } else {
+            System.out.println("Данный номер " + textIn + " уже есть в справочнике");
+        }
+    }
+
+    private void addName(String textIn) {
+        if (!phoneBook.containsValue(textIn)) {
+            System.out.println("Введите номер телефона для абонента " + textIn);
+            String phone = new Scanner(System.in).nextLine();
+            phoneBook.put(phone, textIn);
+        } else {
+            System.out.println("Абонент с таким именем " + textIn + " уже есть в справочнике");
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneBook{" +
+                "phoneBook=" + phoneBook +
+                '}';
+    }
 }
+
+
